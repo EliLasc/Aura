cb41e26c4a99cbc99d42c319223f573a8662e81c
EliLas <Elijahlascurain95@gmail.com>
2024-08-19 19:00:24 -0700

    Section 2 - part 6

--- Aura_UE_Version_5_3/Aura/Intermediate/Build/Win64/UnrealEditor/Inc/Aura/UHT/Aura.init.gen.cpp
+++ Aura_UE_Version_5_3/Aura/Intermediate/Build/Win64/UnrealEditor/Inc/Aura/UHT/Aura.init.gen.cpp
@@ -17,13 +17,13 @@ void EmptyLinkFunctionForGeneratedCodeAura_init() {}
 				nullptr,
 				0,
 				PKG_CompiledIn | 0x00000000,
-				0x244A240D,
-				0xD4ADF32E,
+				0x32C9E07E,
+				0x593FB363,
 				METADATA_PARAMS(0, nullptr)
 			};
 			UECodeGen_Private::ConstructUPackage(Z_Registration_Info_UPackage__Script_Aura.OuterSingleton, PackageParams);
 		}
 		return Z_Registration_Info_UPackage__Script_Aura.OuterSingleton;
 	}
-	static FRegisterCompiledInInfo Z_CompiledInDeferPackage_UPackage__Script_Aura(Z_Construct_UPackage__Script_Aura, TEXT("/Script/Aura"), Z_Registration_Info_UPackage__Script_Aura, CONSTRUCT_RELOAD_VERSION_INFO(FPackageReloadVersionInfo, 0x244A240D, 0xD4ADF32E));
+	static FRegisterCompiledInInfo Z_CompiledInDeferPackage_UPackage__Script_Aura(Z_Construct_UPackage__Script_Aura, TEXT("/Script/Aura"), Z_Registration_Info_UPackage__Script_Aura, CONSTRUCT_RELOAD_VERSION_INFO(FPackageReloadVersionInfo, 0x32C9E07E, 0x593FB363));
 PRAGMA_ENABLE_DEPRECATION_WARNINGS
