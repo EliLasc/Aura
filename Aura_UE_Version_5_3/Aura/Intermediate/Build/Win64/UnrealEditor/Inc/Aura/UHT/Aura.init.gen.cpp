4027da3907c7ed9202f10f3934c9ab986e7cc435
Elijah Lascurain <elijahlascurain95@gmail.com>
2024-10-07 10:09:46 -0700

    First Commit After Unreal Build on PC with .gitignore for PCH File

--- Aura_UE_Version_5_3/Aura/Intermediate/Build/Win64/UnrealEditor/Inc/Aura/UHT/Aura.init.gen.cpp
+++ Aura_UE_Version_5_3/Aura/Intermediate/Build/Win64/UnrealEditor/Inc/Aura/UHT/Aura.init.gen.cpp
@@ -27,7 +27,7 @@ void EmptyLinkFunctionForGeneratedCodeAura_init() {}
 				SingletonFuncArray,
 				UE_ARRAY_COUNT(SingletonFuncArray),
 				PKG_CompiledIn | 0x00000000,
-				0x57E4FE10,
+				0xB16D71AE,
 				0xCBFC1DF1,
 				METADATA_PARAMS(0, nullptr)
 			};
@@ -35,5 +35,5 @@ void EmptyLinkFunctionForGeneratedCodeAura_init() {}
 		}
 		return Z_Registration_Info_UPackage__Script_Aura.OuterSingleton;
 	}
-	static FRegisterCompiledInInfo Z_CompiledInDeferPackage_UPackage__Script_Aura(Z_Construct_UPackage__Script_Aura, TEXT("/Script/Aura"), Z_Registration_Info_UPackage__Script_Aura, CONSTRUCT_RELOAD_VERSION_INFO(FPackageReloadVersionInfo, 0x57E4FE10, 0xCBFC1DF1));
+	static FRegisterCompiledInInfo Z_CompiledInDeferPackage_UPackage__Script_Aura(Z_Construct_UPackage__Script_Aura, TEXT("/Script/Aura"), Z_Registration_Info_UPackage__Script_Aura, CONSTRUCT_RELOAD_VERSION_INFO(FPackageReloadVersionInfo, 0xB16D71AE, 0xCBFC1DF1));
 PRAGMA_ENABLE_DEPRECATION_WARNINGS
--- Aura_UE_Version_5_3/Aura/Intermediate/Build/Win64/x64/AuraEditor/Development/Engine/SharedPCH.Engine.Cpp17.h
+++ Aura_UE_Version_5_3/Aura/Intermediate/Build/Win64/x64/AuraEditor/Development/Engine/SharedPCH.Engine.Cpp17.h
@@ -1,3 +1,3 @@
 // PCH for Runtime/Engine/Public/EngineSharedPCH.h
-#include "C:/Elijah/GASProject/Aura/Aura_UE_Version_5_3/Aura/Intermediate/Build/Win64/x64/AuraEditor/Development/Engine/SharedDefinitions.Engine.Cpp17.h"
+#include "H:/NewAura/Aura/Aura_UE_Version_5_3/Aura/Intermediate/Build/Win64/x64/AuraEditor/Development/Engine/SharedDefinitions.Engine.Cpp17.h"
 #include "Runtime/Engine/Public/EngineSharedPCH.h"
