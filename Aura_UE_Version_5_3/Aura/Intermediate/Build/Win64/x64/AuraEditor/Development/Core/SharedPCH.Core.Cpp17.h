b9ed28b29697180ea223ffd70a72db6e95a7457d
Elijah Lascurain <elijahlascurain95@gmail.com>
2025-02-05 12:00:22 -0800

    Section 9 - part 92

--- Aura_UE_Version_5_3/Aura/Intermediate/Build/Win64/x64/AuraEditor/Development/Core/SharedPCH.Core.Cpp17.h
+++ Aura_UE_Version_5_3/Aura/Intermediate/Build/Win64/x64/AuraEditor/Development/Core/SharedPCH.Core.Cpp17.h
@@ -1,3 +1,3 @@
 // PCH for Runtime/Core/Public/CoreSharedPCH.h
-#include "C:/Elijah/GASProject/Aura/Aura_UE_Version_5_3/Aura/Intermediate/Build/Win64/x64/AuraEditor/Development/Core/SharedDefinitions.Core.Cpp17.h"
+#include "G:/GasProject/Aura/Aura_UE_Version_5_3/Aura/Intermediate/Build/Win64/x64/AuraEditor/Development/Core/SharedDefinitions.Core.Cpp17.h"
 #include "Runtime/Core/Public/CoreSharedPCH.h"
---
+++ Aura_UE_Version_5_3/Aura/Source/Aura/Private/UI/WidgetController/AttributeMenuWidgetController.cpp
@@ -0,0 +1,14 @@
+// Cirquitry Studios
+
+
+#include "UI/WidgetController/AttributeMenuWidgetController.h"
+
+void UAttributeMenuWidgetController::BindCallbacksToDependencies()
+{
+	
+}
+
+void UAttributeMenuWidgetController::BroadcastInitialValues()
+{
+	
+}
